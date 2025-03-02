5cec634bb4bb5b59abbfc56f7b883374c4c8cc2a
angelmello <angelmello0@gmail.com>
2025-03-02 13:58:26 -0600

    2 de marzo

--- contact.php
+++ contact.php
@@ -19,9 +19,10 @@
     </section><!--/#contact-page-->
 
     <div class="map">
-		<iframe src="">
-		</iframe>
-	</div> 
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.825231574701!2d-99.05016928572158!3d19.389913132606897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fd1d9b7df0a9%3A0x45bc6d3961bcf94a!2sLic.%20Juan%20C.%20Doria%2010%2C%20Juan%20Escutia%2C%20Iztapalapa%2C%2009100%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1675355125297!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+
+
 
 	<?php include("components/footer.php");
     include("components/boton-whats.php");?>
